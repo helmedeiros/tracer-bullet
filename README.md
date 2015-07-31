65a673b4d231f066b143f1b7f9ac56316d406785
Sarah Caixeta <sarah_caixeta@gap.com>
2015-07-31 14:09:43 -0700

    Method to show story diff

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# ds-scripts
-Day-by-day Discount Service scripts
+tracer-bullet
+===
 
-Usage:
-  * story_files ITLPP-1234
-    shows all the files changed in story 1234.
-  * story_commits ITLPP-1234
-    shows all the commits in story 1234.
+Shell script project to deal with day-by-day agile projects tracing activities.
--- git_logs.sh
+++ git_logs.sh
@@ -4,6 +4,12 @@ function story_files() {
   git log --oneline --grep="$story_number" --name-only | grep -Eo "\w+/.*\.\w+" | sort -u
 }
 
-function story_commits(){
-  git log --pretty=format:"%C(yellow)%ad%Creset %C(green)%an%Creset %s %C(yellow)%h%Creset" --date=short --grep="$1" | sort -u
+function story_commits() {
+  git log --pretty=format:"%C(yellow)%ad%Creset %C(green)%an%Creset %s %C(yellow)%h%Creset" --date=short --grep= "$1" | sort -u
+}
+
+function story_diff() {
+  first_log=$(git log --pretty=format:%h --grep="$1" | tail -n 1)
+  last_log=$(git log --pretty=format:%h --reverse --grep="$1" | tail -n 1)
+  git difftool -y $first_log $last_log
 }
