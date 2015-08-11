e9151ff2604f7ce3b733dcf37e44491385cdcd1f
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-08-11 14:57:15 -0300

    Solves #5. Showing the menu using command --help (@helmedeiros)

--- pemScripts.sh
+++ pemScripts.sh
@@ -6,7 +6,15 @@ function handle_options(){
   # The user didn't specify a command; give them help
   if [ $# = 0 ]; then
     list_commands;
-  fi
+  else
+  while [ $# -gt 0 ]; do
+    arg=$1;
+    case $arg in
+      "--help" | *) list_commands;
+      break;;
+    esac
+  done
+fi
 }
 
 handle_options $@;
