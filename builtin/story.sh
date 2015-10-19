c91589c53902b0a18fbedf715ca0484452692c9e
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-10-19 11:34:08 -0200

    Solve #28 disconsider deleted files from story:files

--- builtin/story.sh
+++ builtin/story.sh
@@ -64,11 +64,19 @@ function find_missing_tests_for() {
 function story_files() {
   echo "Listing $PROJECT_PREFIX-$1 Files";
 
-  files=$(git log --diff-filter=ACMRTUXB --oneline --grep="$PROJECT_PREFIX"-"$1" --name-only | grep -Eo "\w+/.*\.\w+" | sort -u | grep "$2")
+  allfiles=$(git log --diff-filter=ACMRTUXB --oneline --grep="$PROJECT_PREFIX"-"$1" --name-only | grep -Eo "\w+/.*\.\w+" | sort -u | grep "$2")
+  deletedFiles=$(git log --diff-filter=D --oneline --grep="$PROJECT_PREFIX"-"$1" --name-only | grep -Eo "\w+/.*\.\w+" | sort -u )
 
-  echo "$files"
+  while read -r file; do
+    if [ `echo $deletedFiles | grep -c "$file" ` -le 0 ]; then
+      okLines+=$(printf '\n %s \n' "$file")
+    fi
+  done <<< "$allfiles"
 
-  files_summary "$files"
+
+  echo "$okLines"
+
+  files_summary "$okLines"
 }
 
 function story_diary() {
